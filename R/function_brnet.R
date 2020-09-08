7adb021a6488d8318c0c0397cc4c455677af85cc
AkiraTerui <hanabi0111@gmail.com>
2020-09-08 16:06:00 -0400

    Edits: function_brnet.R
    improve scripts based on lintr::lint_package()

--- R/function_brnet.R
+++ R/function_brnet.R
@@ -1,5 +1,8 @@
 #' Generate a random branching network
 #'
+#' A function generating a random branching network.
+#' The function produces a set of branches that are organized into a branching network. The branch length is drawn from a geometric distribution with branching probability \code{p_branch}.
+#'
 #' @param n_patch number of patches in a network.
 #' @param p_branch branching probability (success probability of a geometric distribution).
 #' @param min_env minimum value of environmental condition in source streams (minimum of a uniform distribution).
@@ -16,6 +19,9 @@
 #' @return \code{branch_id} branch id for each patch.
 #'
 #' @importFrom dplyr %>%
+#' @importFrom grDevices grey
+#' @importFrom graphics par text
+#' @importFrom stats complete.cases rbinom rgeom rnorm runif
 #'
 #' @author Akira Terui, \email{hanabi0111@gmail.com}
 #'
@@ -31,158 +37,167 @@ brnet <- function(n_patch = 100,
                   rho = 1,
                   sd_env = 0.1,
                   randomize_patch = TRUE,
-                  plot = TRUE){
+                  plot = TRUE) {
 
   # define functions and variables ------------------------------------------
 
   resample <- function(x, ...) x[sample.int(length(x), ...)]
 
-  fun_adj <- function(n, start_id = 1){
-    if(n == 1){ Y <- cbind(1, NA) }
-    if(n == 2){
-      Y <- cbind(c(1, 2), c(2, 1))
+  fun_adj <- function(n, start_id = 1) {
+
+    if (n == 1) {
+      m_y <- cbind(1, NA)
     }
-    if(n > 2){
-      y1 <- c(1, rep(2:(n-1), each = 2), n)
-      y2 <- c(2, sapply(2:(n-1), function(i) c(i-1, i+1)), n-1)
-      Y <- cbind(y1, y2)
+
+    if (n == 2) {
+      m_y <- cbind(c(1, 2), c(2, 1))
     }
-    colnames(Y) <- c("patch_1", "patch_2")
-    Y <- Y + (start_id - 1)
-    return(Y)
-  }# fun_adj
+
+    if (n > 2) {
+      y1 <- c(1, rep(2:(n - 1), each = 2), n)
+      y2 <- c(2, sapply(2:(n - 1), function(i) c(i - 1, i + 1)), n - 1)
+      m_y <- cbind(y1, y2)
+    }
+
+    colnames(m_y) <- c("patch_1", "patch_2")
+    m_y <- m_y + (start_id - 1)
+    return(m_y)
+  }
 
 
-  if(p_branch > 0 & p_branch < 1){
-    repeat{
+  if (p_branch > 0 & p_branch < 1) {
+    repeat {
       n_branch <- rbinom(n = 1, size = n_patch, prob = p_branch)
-      if(n_branch%%2 == 1) break
-    }# repeat
-  }else{
-    if(p_branch == 0){ n_branch <- 1 }
-    if(p_branch == 1){
-      if(n_patch%%2 == 0) stop("n_patch must be an odd number when p_branch = 1")
+      if (n_branch %% 2 == 1) break
+    }
+  } else {
+    if (p_branch == 0) {
+      n_branch <- 1
+    }
+
+    if (p_branch == 1) {
+      if (n_patch %% 2 == 0) stop("n_patch must be an odd number when p_branch = 1")
       n_branch <- n_patch
     }
-  }# ifelse
+  }
 
 
   # adjacency matrix: linear network ----------------------------------------
 
-  if(p_branch == 0|n_branch == 1){
-    Adj <- matrix(0, nrow = n_patch, ncol = n_patch)
-    x <- 1:(n_patch-1)
+  if (p_branch == 0 | n_branch == 1) {
+    m_adj <- matrix(0, nrow = n_patch, ncol = n_patch)
+    x <- 1:(n_patch - 1)
     y <- 2:n_patch
-    Adj[cbind(x,y)] <- 1
-    Adj[cbind(y,x)] <- 1
-  }# if(p_branch == 0|n_branch == 1)
+    m_adj[cbind(x, y)] <- 1
+    m_adj[cbind(y, x)] <- 1
+  }# if (p_branch == 0|n_branch == 1)
 
 
   # adjacency matrix: branched network --------------------------------------
 
-  if(p_branch > 0 & n_branch > 1){
+  if (p_branch > 0 & n_branch > 1) {
 
     # vector of the number of patches in each branch
     repeat{
       repeat{
         v_n_patch_branch <- rgeom(n = n_branch, prob = p_branch) + 1
-        if(sum(v_n_patch_branch) >= n_patch) break
+        if (sum(v_n_patch_branch) >= n_patch) break
       }# repeat
-      if(sum(v_n_patch_branch) == n_patch) break
+      if (sum(v_n_patch_branch) == n_patch) break
     }# repeat
 
     # start_id, end_id, and neighbor list for each branch
     v_end_id <- cumsum(v_n_patch_branch)
     v_start_id <- v_end_id - (v_n_patch_branch - 1)
-    list_neighbor_branch <- lapply(1:n_branch, function(i) fun_adj(n = v_n_patch_branch[i], start_id = v_start_id[i]) )
-    Neighbor_branch <- do.call(rbind, list_neighbor_branch)
+    list_neighbor_inbranch <- lapply(1:n_branch, function(i) fun_adj(n = v_n_patch_branch[i], start_id = v_start_id[i]))
+    m_neighbor_inbranch <- do.call(rbind, list_neighbor_inbranch)
 
     # combine parent and offspring branches at confluences
-    if(n_branch == 3){
+    if (n_branch == 3) {
       parent <- c(1, 1)
       offspg <- c(2, 3)
-      Po <- cbind(parent, offspg)
-      list_confluence <- lapply(1:nrow(Po), function(x){ cbind(v_end_id[Po[x,1]], v_start_id[Po[x,2]]) })
-      Confluence <- do.call(rbind, list_confluence)
-      Confluence <- rbind(Confluence, Confluence[,c(2,1)])
+      m_po <- cbind(parent, offspg)
+      list_confluence <- lapply(seq_len(nrow(m_po)), function(x) cbind(v_end_id[m_po[x, 1]], v_start_id[m_po[x, 2]]))
+      m_confluence <- do.call(rbind, list_confluence)
+      m_confluence <- rbind(m_confluence, m_confluence[, c(2, 1)])
     }else{
-      n_confluence <- 0.5*(n_branch - 1)
+      n_confluence <- 0.5 * (n_branch - 1)
       v_parent_branch <- 1:n_confluence
       v_offspg_branch <- 2:n_branch
 
-      Offspg <- matrix(NA, nrow = 2, ncol = n_confluence)
-      for(i in n_confluence:1){
+      m_offspg <- matrix(NA, nrow = 2, ncol = n_confluence)
+      for (i in n_confluence:1) {
         v_y <- resample(v_offspg_branch[v_offspg_branch > v_parent_branch[i]], size = 2)
         v_offspg_branch <- setdiff(v_offspg_branch, v_y)
-        Offspg[,i] <- v_y
+        m_offspg[, i] <- v_y
       }
 
       parent <- rep(v_parent_branch, each = 2)
-      offspg <- c(Offspg)
-      Po <- cbind(parent, offspg)
+      offspg <- c(m_offspg)
+      m_po <- cbind(parent, offspg)
 
-      list_confluence <- lapply(1:nrow(Po), function(x){ cbind(v_end_id[Po[x,1]], v_start_id[Po[x,2]]) })
-      Confluence <- do.call(rbind, list_confluence)
-      Confluence <- rbind(Confluence, Confluence[,c(2,1)])
+      list_confluence <- lapply(seq_len(nrow(m_po)), function(x) cbind(v_end_id[m_po[x, 1]], v_start_id[m_po[x, 2]]))
+      m_confluence <- do.call(rbind, list_confluence)
+      m_confluence <- rbind(m_confluence, m_confluence[, c(2, 1)])
     }# ifelse
 
-    Neighbor <- rbind(Neighbor_branch, Confluence)
-    Neighbor <- Neighbor[complete.cases(Neighbor),]
+    m_neighbor_patch <- rbind(m_neighbor_inbranch, m_confluence)
+    m_neighbor_patch <- m_neighbor_patch[complete.cases(m_neighbor_patch), ]
 
-    Adj <- matrix(0, nrow = n_patch, ncol = n_patch)
-    Adj[Neighbor] <- 1
-  }# if(p_branch > 0)
+    m_adj <- matrix(0, nrow = n_patch, ncol = n_patch)
+    m_adj[m_neighbor_patch] <- 1
+  }# if (p_branch > 0)
 
 
   # distance matrix ---------------------------------------------------------
 
-  D <- matrix(0, ncol = n_patch, nrow = n_patch)
-  A <- diag(x = 1, nrow = n_patch, ncol = n_patch)
-  for(i in 1:n_patch){
-    A <- A%*%Adj
-    D[A!=0 & D==0] <- i
-    if(length(which(D==0)) == 0) break
-  }# for(i in 1:n_patch)
-  diag(D) <- 0
-  A <- NULL
+  m_distance <- matrix(0, ncol = n_patch, nrow = n_patch)
+  m_identity <- diag(x = 1, nrow = n_patch, ncol = n_patch)
+
+  for (i in 1:n_patch) {
+    m_identity <- m_identity %*% m_adj
+    m_distance[m_identity != 0 & m_distance == 0] <- i
+    if (length(which(m_distance == 0)) == 0) break
+  }
+
+  diag(m_distance) <- 0
+  m_identity <- NULL
 
 
   # upstream watershed area ------------------------------------------------
 
-  Adj_up <- Adj
-  Adj_up[lower.tri(Adj_up)] <- 0
+  m_adj_up <- m_adj
+  m_adj_up[lower.tri(m_adj_up)] <- 0
+  m_wa <- matrix(0, ncol = n_patch, nrow = n_patch)
+  m_identity <- diag(1, nrow = n_patch, ncol = n_patch)
 
-  Wm <- matrix(0, ncol = n_patch, nrow = n_patch)
-  A <- diag(1, nrow = n_patch, ncol = n_patch)
+  for (i in 1:n_patch) {
+    m_identity <- m_identity %*% m_adj_up
+    m_wa[m_identity != 0 & m_wa == 0] <- 1
+    if (length(which(m_wa[upper.tri(m_wa)] == 0)) == 0) break
+  }
 
-  for(i in 1:n_patch){
-    A <- A%*%Adj_up
-    Wm[A!=0 & Wm==0] <- 1
-    if(length(which(Wm[upper.tri(Wm)]==0)) == 0) break
-  }# for(i in 1:n_patch)
-  diag(Wm) <- 1
-
-  v_wa <- rowSums(Wm)
+  diag(m_wa) <- 1
+  v_wa <- rowSums(m_wa)
 
 
   # environmental condition -------------------------------------------------
 
-  Pm <- t(apply(X = Adj_up, MARGIN = 1, function(x) x*v_wa/ifelse(sum(x)==0, 1, sum(x*v_wa) ) ) )
-
-  n_source <- 0.5*(n_branch + 1)
-  source <- which(rowSums(Adj_up)==0)
+  m_wa_prop <- t(apply(X = m_adj_up, MARGIN = 1, function(x) (x * v_wa) / ifelse(sum(x) == 0, 1, sum(x * v_wa))))
+  n_source <- 0.5 * (n_branch + 1)
+  source <- which(rowSums(m_adj_up) == 0)
   v_z_dummy <- v_z <- v_env <- rep(0, n_patch)
   v_z[source] <- v_env[source] <- runif(n_source, min = min_env, max = max_env)
   v_z_dummy[source] <- 1
 
-  if(!(rho <= 1&rho >= 0)) stop("rho must be between 0 and 1")
-  for(i in 1:max(D[1,]) ){
+  if (!(rho <= 1 & rho >= 0)) stop("rho must be between 0 and 1")
+  for (i in 1:max(m_distance[1, ])) {
     v_eps <- rep(0, n_patch)
     v_eps[v_z_dummy != 0] <- rnorm(n = length(v_eps[v_z_dummy != 0]), mean = 0, sd = sd_env)
-    v_z <- Pm%*%((rho*v_z) + v_eps)
-    v_z_dummy <- Pm%*%v_z_dummy
+    v_z <- m_wa_prop %*% ((rho * v_z) + v_eps)
+    v_z_dummy <- m_wa_prop %*% v_z_dummy
     v_env <- v_z + v_env
-  }# for(i in 1:max(D[1,]) )
+  }# for(i in 1:max(m_distance[1,]) )
 
 
   # randomize nodes ---------------------------------------------------------
@@ -190,30 +205,30 @@ brnet <- function(n_patch = 100,
   branch <- unlist(lapply(1:n_branch, function(x) rep(x, each = v_n_patch_branch[x])))
   patch <- 1:n_patch
 
-  if(randomize_patch == TRUE){
-    if(n_branch > 1){
+  if (randomize_patch == TRUE) {
+    if (n_branch > 1) {
       df_id <- data.frame(branch = as.character(c(1, resample(2:n_branch)))) %>%
         dplyr::left_join(data.frame(patch, branch = as.character(branch)), by = "branch")
       v_wa <- v_wa[df_id$patch]
       v_env <- v_env[df_id$patch]
-      Adj <- Adj[df_id$patch, df_id$patch]
-      D <- D[df_id$patch, df_id$patch]
+      m_adj <- m_adj[df_id$patch, df_id$patch]
+      m_distance <- m_distance[df_id$patch, df_id$patch]
     }else{
       df_id <- data.frame(branch = 1, patch = 1:n_patch)
-    }# ifelse(n_branch > 1)
+    }
   }else{
     df_id <- data.frame(branch = branch, patch = patch)
-  }# ifelse(randomize_patch == TRUE)
+  }
 
   # visualization -----------------------------------------------------------
 
-  if(plot == T){
-    adj <- igraph::graph.adjacency(adjmatrix = Adj, mode = "undirected")
-    colvalue <- data.frame(color = viridis::viridis(n_patch, alpha = 0.6), value = sort(v_env) )
-    layout.tree <- igraph::layout_as_tree(adj, root = 1, flip.y = F)
-    par(mar = c(5,8,4,3))
-    igraph::plot.igraph(adj, layout = layout.tree,
-                        vertex.size = I(scale(v_wa, center = min(v_wa), scale = max(v_wa)-min(v_wa)) + 0.3)*10,
+  if (plot == T) {
+    adj <- igraph::graph.adjacency(adjmatrix = m_adj, mode = "undirected")
+    colvalue <- data.frame(color = viridis::viridis(n_patch, alpha = 0.6), value = sort(v_env))
+    layout_tree <- igraph::layout_as_tree(adj, root = 1, flip.y = F)
+    par(mar = c(5, 8, 4, 3))
+    igraph::plot.igraph(adj, layout = layout_tree,
+                        vertex.size = I(scale(v_wa, center = min(v_wa), scale = max(v_wa) - min(v_wa)) + 0.3) * 10,
                         vertex.label = NA,
                         vertex.frame.color = grey(0.5),
                         vertex.color = colvalue$color[match(v_env, colvalue$value)])
@@ -221,11 +236,11 @@ brnet <- function(n_patch = 100,
                                   pos = 0.8, side = 2, dec = 2)
     pc <- c(plotfunctions::getCoords(0, side = 1), plotfunctions::getCoords(1, side = 2))
     text(x = pc[1], y = pc[2], labels = "Environmental value", adj = 1)
-  }# if(plot == T)
+  }
 
-  return(list(adjacency_matrix = Adj,
-              distance_matrix = D,
+  return(list(adjacency_matrix = m_adj,
+              distance_matrix = m_distance,
               environment = c(v_env),
               watershed_area = c(v_wa),
-              branch_id = as.numeric(df_id$branch)) )
+              branch_id = as.numeric(df_id$branch)))
 }
