d9138e7dfd000f17910a133793e54df53f4a8820
AkiraTerui <hanabi0111@gmail.com>
2021-05-03 11:57:43 -0400

    Update buildpackage.R

--- tools/buildpackage.R
+++ tools/buildpackage.R
@@ -1,6 +1,15 @@
-usethis::use_mit_license(name = "Akira Terui")
-usethis::use_roxygen_md()
-usethis::use_package_doc()
-devtools::document()
-devtools::load_all()
-devtools::check()
+
+# build package -----------------------------------------------------------
+
+  #usethis::use_mit_license(name = "Akira Terui")
+  usethis::use_roxygen_md()
+  usethis::use_package_doc()
+  devtools::document()
+  devtools::load_all()
+  devtools::check()
+
+
+# check syntax ------------------------------------------------------------
+
+  #usethis::use_coverage()
+  covr::package_coverage()
