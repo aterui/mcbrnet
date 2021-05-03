e21618c06bfbabd39b4d4fcdb01f4ee6279b8a72
AkiraTerui <hanabi0111@gmail.com>
2021-05-03 12:36:05 -0400

    add: function adjtodist

---
+++ tests/testthat/test-adjtodist.R
@@ -0,0 +1,18 @@
+
+# setup -------------------------------------------------------------------
+
+  context("adjtodist")
+
+  n_patch <- 10
+  net <- brnet(n_patch = n_patch,
+               p_branch = 0)
+
+  x <- net$adjacency_matrix
+
+# test --------------------------------------------------------------------
+
+  test_that("check adjtodist output", {
+
+    expect_equal(max(adjtodist(x)), n_patch - 1)
+
+  })
