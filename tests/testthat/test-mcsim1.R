c7abf8e1ff703996258ecdac15184f44b6b7ad8d
AkiraTerui <hanabi0111@gmail.com>
2021-05-03 00:28:48 -0400

    Create test-mcsim1.R

---
+++ tests/testthat/test-mcsim1.R
@@ -0,0 +1,28 @@
+
+context("mcsim")
+
+n_patch <- 50
+n_species <- 10
+mean_env <- rnorm(n_patch)
+niche_optim <- rnorm(n_species)
+sd_niche_width <- 1:n_species * 0.5
+K <- 1:n_patch
+
+output <- mcsim(n_species = n_species,
+                n_patch = n_patch,
+                mean_env = mean_env,
+                niche_optim = niche_optim,
+                sd_niche_width = sd_niche_width,
+                carrying_capacity = K)
+
+test_that("check environmental/niche parameters", {
+  expect_equal(unique(output$df_dynamics$mean_env), mean_env)
+  expect_equal(unique(output$df_dynamics$niche_optim), niche_optim)
+  expect_equal(unique(output$df_dynamics$carrying_capacity), K)
+
+  expect_equal(output$df_species$niche_optim, niche_optim)
+  expect_equal(output$df_species$sd_niche_width, sd_niche_width)
+  expect_equal(output$df_patch$mean_env, mean_env)
+  expect_equal(output$df_patch$carrying_capacity, K)
+})
+
