2c2c5804cc73a2a76960867323c8802986637706
AkiraTerui <hanabi0111@gmail.com>
2021-05-03 00:46:41 -0400

    Update test-mcsim1.R

--- tests/testthat/test-mcsim1.R
+++ tests/testthat/test-mcsim1.R
@@ -8,14 +8,14 @@
   mean_env <- rnorm(n_patch)
   niche_optim <- rnorm(n_species)
   sd_niche_width <- 1:n_species * 0.5
-  K <- 1:n_patch
+  cc <- 1:n_patch
 
   output <- mcsim(n_species = n_species,
                   n_patch = n_patch,
                   mean_env = mean_env,
                   niche_optim = niche_optim,
                   sd_niche_width = sd_niche_width,
-                  carrying_capacity = K)
+                  carrying_capacity = cc)
 
 
 # test --------------------------------------------------------------------
@@ -26,11 +26,10 @@
 
     expect_equal(unique(output$df_dynamics$mean_env), mean_env)
     expect_equal(unique(output$df_dynamics$niche_optim), niche_optim)
-    expect_equal(unique(output$df_dynamics$carrying_capacity), K)
+    expect_equal(unique(output$df_dynamics$carrying_capacity), cc)
 
     expect_equal(output$df_species$niche_optim, niche_optim)
     expect_equal(output$df_species$sd_niche_width, sd_niche_width)
     expect_equal(output$df_patch$mean_env, mean_env)
-    expect_equal(output$df_patch$carrying_capacity, K)
+    expect_equal(output$df_patch$carrying_capacity, cc)
   })
-
