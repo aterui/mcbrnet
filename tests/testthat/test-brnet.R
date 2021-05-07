a0eafa52e4d676da0de1020daa292c5150f995a0
aterui <hanabi0111@gmail.com>
2021-05-07 15:11:40 -0400

    Update test-brnet.R

--- tests/testthat/test-brnet.R
+++ tests/testthat/test-brnet.R
@@ -4,7 +4,7 @@
   context("brnet")
 
   n_patch <- 10
-  net <- brnet(n_patch = n_patch,
+  net1 <- brnet(n_patch = n_patch,
                p_branch = 0.5,
                sd_env_lon = 0,
                sd_env_source = 0,
@@ -12,6 +12,15 @@
                mean_disturb_source = 0.9,
                sd_disturb_source = .1,
                patch_label = "patch")
+
+  net2 <- brnet(n_patch = n_patch,
+                p_branch = 0.5,
+                sd_env_lon = 0,
+                sd_env_source = 0,
+                mean_env_source = 10,
+                mean_disturb_source = 0.9,
+                sd_disturb_source = 0,
+                patch_label = "patch")
 
   linear <- brnet(n_patch = n_patch,
                   p_branch = 0,
@@ -23,9 +32,11 @@
   test_that("check brnet output", {
     expect_error(brnet(n_patch = 100, p_branch = 1))
 
-    expect_equal(unique(net$df_patch$disturbance), 0.9)
+    expect_equal(n_distinct(net1$df_patch$disturbance),
+                 n_distinct(net1$df_patch$branch_id))
+    expect_equal(unique(net2$df_patch$disturbance), 0.9)
     expect_equal(dplyr::n_distinct(linear$df_patch$disturbance), 1)
-    expect_equal(net$df_patch$n_patch_upstream[1], n_patch)
-    expect_equal(unique(round(net$df_patch$environment, 2)), 10)
+    expect_equal(net1$df_patch$n_patch_upstream[1], n_patch)
+    expect_equal(unique(round(net1$df_patch$environment, 2)), 10)
     expect_equal(max(linear$distance_matrix), n_patch - 1)
   })
