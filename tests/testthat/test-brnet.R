72bcdfeaab4c047b914178580b3c2970442ce652
AkiraTerui <hanabi0111@gmail.com>
2021-05-07 17:31:28 -0400

    Update test-brnet.R

--- tests/testthat/test-brnet.R
+++ tests/testthat/test-brnet.R
@@ -32,8 +32,8 @@
   test_that("check brnet output", {
     expect_error(brnet(n_patch = 100, p_branch = 1))
 
-    expect_equal(n_distinct(net1$df_patch$disturbance),
-                 n_distinct(net1$df_patch$branch_id))
+    expect_equal(dplyr::n_distinct(net1$df_patch$disturbance),
+                 dplyr::n_distinct(net1$df_patch$branch_id))
     expect_equal(unique(net2$df_patch$disturbance), 0.9)
     expect_equal(dplyr::n_distinct(linear$df_patch$disturbance), 1)
     expect_equal(net1$df_patch$n_patch_upstream[1], n_patch)
