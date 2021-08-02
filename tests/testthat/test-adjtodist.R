21eff5d57f94f85ffd41e1c619a6fc911c18a48c
aterui <hanabi0111@gmail.com>
2021-08-02 10:28:18 -0400

    edits: test files

--- tests/testthat/test-adjtodist.R
+++ tests/testthat/test-adjtodist.R
@@ -1,18 +1,18 @@
 
 # setup -------------------------------------------------------------------
 
-  context("adjtodist")
+context("adjtodist")
 
-  n_patch <- 10
-  net <- brnet(n_patch = n_patch,
-               p_branch = 0)
+n_patch <- 10
+net <- brnet(n_patch = n_patch,
+             p_branch = 0)
 
-  x <- net$adjacency_matrix
+x <- net$adjacency_matrix
 
 # test --------------------------------------------------------------------
 
-  test_that("check adjtodist output", {
+test_that("check adjtodist output", {
 
-    expect_equal(max(adjtodist(x)), n_patch - 1)
+  expect_equal(max(adjtodist(x)), n_patch - 1)
 
-  })
+})
--- tests/testthat/test-brnet.R
+++ tests/testthat/test-brnet.R
@@ -1,42 +1,43 @@
 
 # setup -------------------------------------------------------------------
 
-  context("brnet")
+context("brnet")
 
-  n_patch <- 10
-  net1 <- brnet(n_patch = n_patch,
-               p_branch = 0.5,
-               sd_env_lon = 0,
-               sd_env_source = 0,
-               mean_env_source = 10,
-               mean_disturb_source = 0.9,
-               sd_disturb_source = .1,
-               patch_label = "patch")
+n_patch <- 10
+net1 <- brnet(n_patch = n_patch,
+              p_branch = 0.5,
+              sd_env_lon = 0,
+              sd_env_source = 0,
+              mean_env_source = 10,
+              mean_disturb_source = 0.9,
+              sd_disturb_source = .1,
+              plot = FALSE)
 
-  net2 <- brnet(n_patch = n_patch,
-                p_branch = 0.5,
-                sd_env_lon = 0,
-                sd_env_source = 0,
-                mean_env_source = 10,
+net2 <- brnet(n_patch = n_patch,
+              p_branch = 0.5,
+              sd_env_lon = 0,
+              sd_env_source = 0,
+              mean_env_source = 10,
+              mean_disturb_source = 0.9,
+              sd_disturb_source = 0,
+              plot = FALSE)
+
+linear <- brnet(n_patch = n_patch,
+                p_branch = 0,
                 mean_disturb_source = 0.9,
-                sd_disturb_source = 0,
-                patch_label = "patch")
-
-  linear <- brnet(n_patch = n_patch,
-                  p_branch = 0,
-                  mean_disturb_source = 0.9,
-                  sd_disturb_source = .1)
+                sd_disturb_source = 0.1,
+                plot = FALSE)
 
 # test --------------------------------------------------------------------
 
-  test_that("check brnet output", {
-    expect_error(brnet(n_patch = 100, p_branch = 1))
+test_that("check brnet output", {
+  expect_error(brnet(n_patch = 100, p_branch = 1))
 
-    expect_equal(dplyr::n_distinct(net1$df_patch$disturbance),
-                 dplyr::n_distinct(net1$df_patch$branch_id))
-    expect_equal(unique(net2$df_patch$disturbance), 0.9)
-    expect_equal(dplyr::n_distinct(linear$df_patch$disturbance), 1)
-    expect_equal(net1$df_patch$n_patch_upstream[1], n_patch)
-    expect_equal(unique(round(net1$df_patch$environment, 2)), 10)
-    expect_equal(max(linear$distance_matrix), n_patch - 1)
-  })
+  expect_equal(dplyr::n_distinct(net1$df_patch$disturbance),
+               dplyr::n_distinct(net1$df_patch$branch_id))
+  expect_equal(unique(round(net2$df_patch$disturbance, 1)), 0.9)
+  expect_equal(dplyr::n_distinct(linear$df_patch$disturbance), 1)
+  expect_equal(net1$df_patch$n_patch_upstream[1], n_patch)
+  expect_equal(unique(round(net1$df_patch$environment, 2)), 10)
+  expect_equal(max(linear$distance_matrix), n_patch - 1)
+})
