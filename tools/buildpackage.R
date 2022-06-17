f2a28cc0bde86a507064a6ee5f60c64f61780f3d
AkiraTerui <hanabi0111@gmail.com>
2022-06-16 23:45:35 -0400

    edit: rebuild

--- tools/buildpackage.R
+++ tools/buildpackage.R
@@ -22,7 +22,7 @@ devtools::check(vignettes=FALSE)
 # build website -----------------------------------------------------------
 
 # Run once to configure package to use pkgdown
-usethis::use_pkgdown()
+#usethis::use_pkgdown()
 
 # Run to build the website
 pkgdown::build_site()
