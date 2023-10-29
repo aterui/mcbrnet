baf43ed423af7e153637f71ac11d09ab86b55363
aterui <hanabi0111@gmail.com>
2023-10-28 22:57:24 -0400

    edit: include cpp option in sglv()

---
+++ src/deriv.cpp
@@ -0,0 +1,42 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins("cpp11")]]
+
+// [[Rcpp::export("deriv")]]
+SEXP deriv(double t, arma::vec n, Rcpp::List parms) {
+
+  // R compatible
+  // deriv <- function(t, n, parms) {
+  //   with(parms, {
+  //     psi <- input(t)
+  //     dn <- n * (r - psi * e + t(A) %*% n) + C %*% n
+  //     list(dn)
+  //   })
+  // }
+
+  // parameters
+  // r: [vector] ns x np vector for intrinsic growth
+  // A: [matrix] (ns x np) x (ns x np) interaction matrix
+  // C: [matrix] (ns x np) x (ns x np) connectivity matrix
+  // e: [vector] ns x np vector for disturbance intensity
+  // psi: [scalar] disturbance incidence
+  arma::vec r = parms["r"];
+  arma::vec e = parms["e"];
+  arma::mat A = parms["A"];
+  arma::mat C = parms["C"];
+
+  // input: approximation function
+  // read from R Global Env
+  Rcpp::Environment e0 = Rcpp::Environment::env("package:mcbrnet");
+  Rcpp::Function input = e0["input"];
+
+  // output
+  arma::vec dn(n.size());
+
+  // model
+  SEXP v = input(t);
+  Rcpp::NumericVector psi(v);
+  dn = n % (r - psi[0] * e + A.t() * n) + C * n;
+
+  return(Rcpp::List::create(dn));
+}
